b9f0d4aead546eb73ead8b9c261dfc56ec4a177a
kumar-shivang <shivangkumar1857@gmail.com>
2024-07-28 00:39:00 +0530

    Refactor Student class to use DictField for grades

--- database/__init__.py
+++ database/__init__.py
@@ -1,14 +1,8 @@
 from mongoengine import connect
-from .tables import Student, Course, Week, Lecture, Question, Assignment, Submission
+
+from .tables import Assignment, Course, Lecture, Question, Student, Submission, Week
+
 
 def init_db():
     connect(db="mydb", host="127.0.0.1", port=27017)
     print("database created")
-
-
-
-
-
-
-
-
