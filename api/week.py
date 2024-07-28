145b6ba72fd42390de2afd35cee8321bb7106c78
kumar-shivang <shivangkumar1857@gmail.com>
2024-07-29 00:01:07 +0530

    feat: Add error handling for retrieving a single week

--- api/week.py
+++ api/week.py
@@ -7,16 +7,19 @@ from . import api
 week_blueprint = Blueprint("week", __name__, url_prefix="/week")
 
 
-@week_blueprint.route("/<course_id>", methods=["GET"])
-def get_weeks(course_id):
-    course = Course.objects(id=course_id).first()
-    weeks = course.weeks
-    return jsonify({"weeks": [week.to_json() for week in weeks]})
+# @week_blueprint.route("/<course_id>", methods=["GET"])
+# def get_weeks(course_id):
+#     course = Course.objects(id=course_id).first()
+#     weeks = course.weeks
+#     return jsonify({"weeks": [week.to_json() for week in weeks]})
 
 
+# NOTE: get single week
 @week_blueprint.route("/<week_id>", methods=["GET"])
 def get_week(week_id):
     week = Week.objects(id=week_id).first()
+    if week is None:
+        return jsonify({"error": "Week not found"}), 404
     return week.to_json()
 
 
