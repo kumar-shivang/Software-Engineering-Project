14c6990db6edb95e3a024b05b4b4db461608d837
kumar-shivang <shivangkumar1857@gmail.com>
2024-07-28 11:40:35 +0530

    Refactor API initialization to use Flask app parameter

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint,Flask
 
 api = Blueprint("api", __name__, url_prefix="/api")
+
+
+def init_api(app:Flask)->None:
+    from .student import student_blueprint
+    from .course import course_blueprint
+    from .week import week_blueprint
+    from .llm import llm_blueprint
+    app.register_blueprint(api)
+    # api.register_blueprint(student_blueprint)
+    # api.register_blueprint(course_blueprint)
+    # api.register_blueprint(llm_blueprint)
+    print(app.url_map)
+
+    
