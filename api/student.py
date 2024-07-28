eaa7ea9b250139b22a48ac1eabea0db0963f63f0
kumar-shivang <shivangkumar1857@gmail.com>
2024-07-29 00:01:01 +0530

    chore: added error handling

--- api/student.py
+++ api/student.py
@@ -7,16 +7,17 @@ from . import api
 student_blueprint = Blueprint("student", __name__, url_prefix="/student")
 
 
+# NOTE: return all students
 @student_blueprint.route("/", methods=["GET"])
 def get_students():
     students = Student.objects().to_json()
     return students
 
 
+# NOTE: assignment submission
 @student_blueprint.route("/submit", methods=["POST"])
 def submit():
     data = request.json
-    print(data)
     student = Student.objects(id=data["student_id"]).first()
     assignment = Assignment.objects(id=data["assignment_id"]).first()
     answers = data["answers"]
@@ -24,19 +25,26 @@ def submit():
     return jsonify({"message": "Submitted"})
 
 
-@student_blueprint.route("/student_id>", methods=["GET"])
+# NOTE: get student by id
+@student_blueprint.route("/<student_id>", methods=["GET"])
 def get_student(student_id):
     student = Student.objects(id=student_id).first()
+    if student is None:
+        return jsonify({"error": "Student not found"}), 404
     return student.to_json()
 
 
+# NOTE: get courses by student id
 @student_blueprint.route("/courses/<student_id>", methods=["GET"])
 def get_courses(student_id):
     student = Student.objects(id=student_id).first()
+    if student is None:
+        return (jsonify({"error": "Student not found"}),)
     courses = student.courses
     return jsonify({"courses": [course.to_json() for course in courses]})
 
 
+# NOTE: get assignments by student id
 @student_blueprint.route("/score", methods=["GET"])
 def get_score(student_id):
     data = request.json
@@ -44,6 +52,10 @@ def get_score(student_id):
     assignment_id = data["assignment_id"]
 
     student = Student.objects(id=student_id).first()
+    if student is None:
+        return jsonify({"error": "Student not found"}), 404
+    if not Assignment.objects(id=assignment_id).first():
+        return jsonify({"error": "Assignment not found"}), 404
     return jsonify({"score": student.get_score(assignment_id)})
 
 
