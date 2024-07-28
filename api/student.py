17ad2b7f08898d870645aed6c52549863d8c355f
kumar-shivang <shivangkumar1857@gmail.com>
2024-07-28 11:41:20 +0530

    chore: Refactor student.py to use Flask Blueprint for routing

--- api/student.py
+++ api/student.py
@@ -4,14 +4,17 @@ from database.tables import Assignment, Student
 
 from . import api
 
+student_blueprint = Blueprint("student", __name__, url_prefix="/student")
 
-@api.route("/student", methods=["GET"])
+
+
+@student_blueprint.route("/", methods=["GET"])
 def get_students():
     students = Student.objects().to_json()
     return students
 
 
-@api.route("student/submit", methods=["POST"])
+@student_blueprint.route("/submit", methods=["POST"])
 def submit():
     data = request.json
     print(data)
@@ -22,20 +25,20 @@ def submit():
     return jsonify({"message": "Submitted"})
 
 
-@api.route("/student/student_id>", methods=["GET"])
+@student_blueprint.route("/student_id>", methods=["GET"])
 def get_student(student_id):
     student = Student.objects(id=student_id).first()
     return student.to_json()
 
 
-@api.route("/student/courses/<student_id>", methods=["GET"])
+@student_blueprint.route("/courses/<student_id>", methods=["GET"])
 def get_courses(student_id):
     student = Student.objects(id=student_id).first()
     courses = student.courses
     return jsonify({"courses": [course.to_json() for course in courses]})
 
 
-@api.route("student/score", methods=["GET"])
+@student_blueprint.route("/score", methods=["GET"])
 def get_score(student_id):
     data = request.json
     student_id = data["student_id"]
@@ -43,3 +46,9 @@ def get_score(student_id):
 
     student = Student.objects(id=student_id).first()
     return jsonify({"score": student.get_score(assignment_id)})
+
+
+api.register_blueprint(student_blueprint)
+
+
+
